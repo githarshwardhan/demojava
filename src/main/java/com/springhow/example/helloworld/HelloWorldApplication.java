0422f16abcacd1eea5d63bd65c18ab08e77dddae
aniketkshirsagar9071 <77712061+aniketkshirsagar9071@users.noreply.github.com>
2021-12-14 10:43:24 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "my name is nikhil khaire,from pune";
+        return "my name is nikhil khaire,from maharashtra";
     }
 
 }
